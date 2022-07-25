39e2af774def52d8a5a9e1bcd82137439e584430
nguyentuan <dungtuann67@gmail.com>
2022-07-25 15:26:23 +0700

    MusicControl components

--- src/layouts/components/SidebarLeft/index.js
+++ src/layouts/components/SidebarLeft/index.js
@@ -1,6 +1,11 @@
+import classNames from "classnames/bind";
+import styles from './SidebarLeft.module.scss'
+
 function SidebarLeft() {
     return ( 
-        <h2>SidebarLeft</h2>
+        <aside>
+
+        </aside>
     );
 }
 
