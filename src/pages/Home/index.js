36749b684d21362d16ecd7bbf49a7d940835b4ed
thanh <nguyenphucthanh1879@gmail.com>
2022-08-04 20:57:17 +0700

    thanh fix Slide

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,16 @@
-import { useEffect, useState } from 'react';
-import {
-    getHome,
-  } from "nhaccuatui-api-full";
-import { Slide } from "react-slideshow-image";
-import classNames from "classnames/bind";
-import styles from './Home.module.scss';
+import { getHome  } from 'nhaccuatui-api-full';
+import classNames from 'classnames/bind';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import { useState, useEffect, useRef, Fragment } from 'react';
+import styles from './Home.module.scss'
 const cx = classNames.bind(styles)
 
-
 function Home() {
-
     const [getSlide, setGetSlide] = useState([]);
 
     useEffect(() => {
-        const getSideHome = async () => {
+        const getSlideHome = async () => {
             try {
                 const res = await getHome()
                 setGetSlide(res.newRelease.song)
@@ -21,48 +18,99 @@ function Home() {
                 alert(error);
             }
         };
-        getSideHome(getSlide);
+        getSlideHome();
     },[])
+
     console.log(getSlide);
 
-    function renderTopSearch() {
-        if(getSlide) {
-            return getSlide.map((item, index) => {
-                return (                 
-                    <p key={index}>      
-                        <img
-                        // className={cx('mt-10', 'mb-5', 'mb-md-0', 'col-md-12')}
-                        src={item?.thumbnail}
-                        alt="..."
-                        />
-                        {/* <div >{item.title}</div> */}
-                    </p>
-                )
-            })
-        }
+
+    const [state, setState] = useState({
+        show1Index: 1,
+        show2Index: 2,
+        show3Index: 3,
+        none1Index: 4,
+        none2Index: 5,
+        none3Index: 6,
+    })
+    const changeSlide = () => {
+        setState({
+            ...state,
+            show1Index: ++state.show1Index > 6 ? 1 : state.show1Index++,
+            show2Index: ++state.show2Index > 6 ? 1 : state.show2Index++,
+            show3Index: ++state.show3Index > 6 ? 1 : state.show3Index++,
+            none1Index: ++state.none1Index > 6 ? 1 : state.none1Index++,
+            none2Index: ++state.none2Index > 6 ? 1 : state.none2Index++,
+            none3Index: ++state.none3Index > 6 ? 1 : state.none3Index++,
+        })
     }
 
-    return (
-        <p className={cx('a')}>
-            {/* <Slide slidesToScroll={1} className='a' slidesToShow={1} indicators={true} autoplay={false} responsive={[{
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-                }, {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-                }]}> */}
+    const changeSlide2 = () => {
+        setState({
+            ...state,
+            show1Index: --state.show1Index < 1 ? 6 : state.show1Index--,
+            show2Index: --state.show2Index < 1 ? 6 : state.show2Index--,
+            show3Index: --state.show3Index < 1 ? 6 : state.show3Index--,
+            none1Index: --state.none1Index < 1 ? 6 : state.none1Index--,
+            none2Index: --state.none2Index < 1 ? 6 : state.none2Index--,
+            none3Index: --state.none3Index < 1 ? 6 : state.none3Index--,
+        })
+    }
 
-                <p>{renderTopSearch()}</p>
-            {/* </Slide> */}
-            
-        </p>
+
+    const Slideshow = () => {
+        if(getSlide){
+            return (
+                getSlide.map((item, index)=> {
+                    let classActive = '';
+                    if (index + 1 === state.show1Index) {
+                        classActive = 'show1';
+                    } else if (index + 1 === state.show2Index) {
+                        classActive = 'show2';
         
+                    } else if (index + 1 === state.show3Index) {
+                        classActive = 'show3';
+        
+                    } else if (index + 1 === state.none1Index) {
+                        classActive = 'hide1';
+        
+                    } else if (index + 1 === state.none2Index) {
+                        classActive = 'hide2';
+        
+                    } else if (index + 1 === state.none3Index) {
+                        classActive = 'hide3';
+                    }
+                    return(
+
+                            <div className={`itemGallary ${classActive}`} key={index}>
+                                <img 
+                                    src={item.thumbnail}   
+                                    alt={index} 
+                                    className='p-3' 
+                                />
+                            </div>
+       
+                    )
+                }) 
+            )
+        }
+    }
+    
+    return (
+        <div className={cx('khamPhaContent')}>
+            <div className='mt-14'>
+                <div className='gallery pt-6'>
+                    <div className= {cx('gallery-content', 'flex w-full')}>
+                        {Slideshow()}
+                        <button className= {cx('galleryButton', 'btn2')} onClick={() => {
+                            changeSlide();
+                        }}><i class="fa fa-arrow-right"></i></button>
+                        <button className={cx('galleryButton', 'btn1')} onClick={() => {
+                            changeSlide2();
+                        }}><i class="fa fa-arrow-left"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
